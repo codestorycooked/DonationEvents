fe199dfd169741c2f460b61636a1431ca63b76e5
codestorycooked <kunal.deshmukh@hotmail.com>
2017-06-15 07:40:19 +0530

    Changes

--- DonationEvents/DataModel.Context.cs
+++ DonationEvents/DataModel.Context.cs
@@ -29,5 +29,10 @@ namespace DonationEvents
         public virtual DbSet<DonationRange> DonationRanges { get; set; }
         public virtual DbSet<Event> Events { get; set; }
         public virtual DbSet<UserDonation> UserDonations { get; set; }
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
+        public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
+        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
+        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
     }
 }
